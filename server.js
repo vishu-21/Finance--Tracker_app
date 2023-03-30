c2d7adc7425ae55fb76bc8f235feb354169303f2
vishu-21 <vishal211199@gmail.com>
2023-03-30 21:21:40 +0530

    submit

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
-const colors = require('colors');
-const connectDB = require('./config/db');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
-dotenv.config({path: './config/config.env'});
+dotenv.config({ path: "./config/config.env" });
 
 connectDB();
 
-const transactions = require('./routes/transactions');
+const transactions = require("./routes/transactions");
 
 const server = express();
 
 server.use(express.json());
 
-if(process.env.NODE_ENV === 'development') {
-    server.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  server.use(morgan("dev"));
 }
 
-server.use('/api/v1/transactions',transactions);
-
+server.use("/api/v1/transactions", transactions);
 
 //static file
+if (process.env.NODE_ENV === 'production') {
+    server.use(express.static('client/build'));
 
- server.use(express.static(path.join(__dirname, './client/build')));
-
- server.get('*', (req,res)=> res.sendFile(path.join(__dirname, './client/build/index.html')));
-
+    server.get('*',(req,res)=> res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 8080;
 
-server.listen(PORT , console.log(`server is running on ${PORT}`.blue.italic));
+server.listen(PORT, console.log(`server is running on ${PORT}`.blue.italic));
